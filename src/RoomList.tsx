ac29c874fa8d2df601fcc5b4045190b5a31899b4
jtuu <joona.tuukkanen@gmail.com>
2018-03-24 22:21:27 +0000

    Added lobby footer.

--- src/RoomList.tsx
+++ src/RoomList.tsx
@@ -5,20 +5,36 @@ import { Bind } from "./Decorators";
 import { Link } from "react-router-dom";
 import { ListGroup, ListGroupItem, Button } from "reactstrap";
 
+<<<<<<< HEAD
 interface RoomListProps {
 
 }
 
 interface RoomListEntry {
+=======
+interface RoomListProps{
+
+}
+
+interface RoomListEntry{
+>>>>>>> c47c0c45ce7672a7abd9b330450138222d348e21
   id: string;
   name: string;
 }
 
+<<<<<<< HEAD
 interface RoomListState {
   rooms: RoomListEntry[];
 }
 
 export class RoomList extends React.Component<RoomListProps, RoomListState> {
+=======
+interface RoomListState{
+  rooms: RoomListEntry[];
+}
+
+export class RoomList extends React.Component<RoomListProps, RoomListState>{
+>>>>>>> c47c0c45ce7672a7abd9b330450138222d348e21
   private roomsRef!: firebase.database.Reference;
   private initialRoomsReceived = false;
 
@@ -86,7 +102,11 @@ export class RoomList extends React.Component<RoomListProps, RoomListState> {
 
     if (this.state.rooms.length < 1) {
       listContents = <em style={{textAlign: "center"}}>None!</em>;
+<<<<<<< HEAD
     } else {
+=======
+    }else {
+>>>>>>> c47c0c45ce7672a7abd9b330450138222d348e21
       listContents = this.state.rooms.map(room => {
         const encodedId = encodeURIComponent(room.id);
         const encodedName = encodeURIComponent(room.name);
@@ -110,7 +130,11 @@ export class RoomList extends React.Component<RoomListProps, RoomListState> {
     }
 
     return (
+<<<<<<< HEAD
+      <div style={{minHeight: "86%"}}>
+=======
       <div>
+>>>>>>> c47c0c45ce7672a7abd9b330450138222d348e21
         <div style={{textAlign: "center"}}>Games currently running:</div>
         <ListGroup>{listContents}</ListGroup>
       </div>
