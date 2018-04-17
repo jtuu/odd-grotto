37b1719cc0aaaeaecece970dd9b7d08f77b3d53e
jtuu <joona.tuukkanen@gmail.com>
2018-04-17 11:33:19 +0000

    Fixed Memoize decorator.

--- src/Decorators.ts
+++ src/Decorators.ts
@@ -1,52 +1,54 @@
 function memoizeMethod(descriptor: TypedPropertyDescriptor<any>) {
   const originalValue = descriptor.value;
-  let hasRun = false;
-  let returnedValue: any;
+  const returnedValues: WeakMap<object, any> = new WeakMap();
 
   descriptor.value = function(...args: any[]) {
-    if (!hasRun) {
-      returnedValue = originalValue.apply(this, args);
-      hasRun = true;
-    }
-
-    return returnedValue;
+      let val;
+      if (returnedValues.has(this)) {
+          val = returnedValues.get(this);
+      } else {
+          val = originalValue.apply(this, args);
+          returnedValues.set(this, val);
+      }
+      return val;
   };
 }
 
 function memoizeGetAccessor(descriptor: TypedPropertyDescriptor<any>) {
   const originalGet = descriptor.get;
   const originalSet = descriptor.set;
-  let hasRun = false;
-  let returnedValue: any;
+  const returnedValues: WeakMap<object, any> = new WeakMap();
 
   if (originalGet !== undefined) {
-    descriptor.get = function(...args: any[]) {
-      if (!hasRun) {
-        returnedValue = originalGet.apply(this, args);
-        hasRun = true;
-      }
-
-      return returnedValue;
-    };
+      descriptor.get = function(...args: any[]) {
+          let val;
+          if (returnedValues.has(this)) {
+              val = returnedValues.get(this);
+          } else {
+              val = originalGet.apply(this, args);
+              returnedValues.set(this, val);
+          }
+          return val;
+      };
   }
 
   if (originalSet !== undefined) {
-    descriptor.set = function(...args: any[]) {
-      hasRun = false;
-      return originalSet.apply(this, args);
-    };
+      descriptor.set = function(...args: any[]) {
+          returnedValues.delete(this);
+          return originalSet.apply(this, args);
+      };
   }
 }
 
-export const Memoize: MethodDecorator = function Memoize(_target, _propertyName, descriptor) {
+export function Memoize<T extends object, K extends keyof T>(_target: T, _propertyName: K, descriptor: TypedPropertyDescriptor<T[K]>) {
   if (descriptor.value !== undefined) {
-    memoizeMethod(descriptor);
+      memoizeMethod(descriptor);
   } else if (descriptor.get !== undefined) {
-    memoizeGetAccessor(descriptor);
+      memoizeGetAccessor(descriptor);
   } else {
-    throw new Error("Only methods or getters can be decorated with Memoize.");
+      throw new Error("Only methods or getters can be decorated with Memoize.");
   }
-};
+}
 
 export const Benchmark: MethodDecorator = function Benchmark(_target, propertyName, descriptor: TypedPropertyDescriptor<any>) {
   const originalValue = descriptor.value;
